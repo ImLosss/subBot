c6fe0f6f8047aaa012d5b94200bce20ef077e8a7
ImLosss <pahriansyahryan@gmail.com>
2025-06-09 15:53:00 +0800

    update

--- app/controller/SwallowedController.js
+++ app/controller/SwallowedController.js
@@ -5,13 +5,13 @@ const { globalUpdate } = require('service/SwallowedService');
 const { deepseek, deepseekTraining } = require('function/deepseek');
 
 const ss = withErrorHandling(async (msg, arg, chat) => {
-    let config = readJSONFileSync(`./config.json`)
+    let config = readJSONFileSync(`./config3.json`)
     let response;
     
     if(!arg) return await chat.sendMessage('Command salah/kurang');
     if(arg.startsWith('updateglobal')) return globalUpdate(arg, chat);
-    else if(arg.startsWith('training')) response = await deepseekTraining(arg, 'database/data_chat/data_chat_ss', config.GLOBAL_CHAT_TRAINING);
-    else response = await deepseek(arg, 'database/training/ss', config.GLOBAL_CHAT_SS);
+    else if(arg.startsWith('training')) response = await deepseekTraining(arg, 'database/data_chat/data_chat_btth_deepseek', config.GLOBAL_CHAT_BTTH_TRAINING2);
+    else response = await deepseek(arg, 'database/training/ss', 'database/training/important/ss.json', config.GLOBAL_CHAT_SS, chat);
     
     await chat.sendMessage(response);
 })
