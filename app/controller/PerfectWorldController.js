36fe95929a84f8ae88fba9d6a1f78be65151fa47
ImLosss <pahriansyahryan@gmail.com>
2025-06-12 19:15:46 +0800

    update

--- app/controller/PerfectWorldController.js
+++ app/controller/PerfectWorldController.js
@@ -5,14 +5,14 @@ const { globalUpdate, eden, edenTraining } = require('service/PerfectWorldServic
 const { deepseek, deepseekTraining } = require('function/deepseek');
 
 const pw = withErrorHandling(async (msg, arg, chat) => {
-    let config = readJSONFileSync(`./config.json`)
+    let config = readJSONFileSync(`./config3.json`)
     let response;
     
     if(!arg) return await chat.sendMessage('Command salah/kurang');
     if(arg.startsWith('updateglobal')) return globalUpdate(arg, chat);
-    else if(arg.startsWith('training')) response = await deepseekTraining(arg, 'database/data_chat/data_chat_pw_deepseek', config.GLOBAL_CHAT_BTTH_TRAINING2);
-    else response = await deepseek(arg, 'database/data_chat/data_chat_pw', config.GLOBAL_CHAT_PW);
-    
+    else if(arg.startsWith('training')) response = await deepseekTraining(arg, 'database/data_chat/data_chat_pw_deepseek', config.GLOBAL_CHAT_PW_TRAINING2);
+    else response = await deepseek(arg, 'database/training/pw', 'database/training/important/pw.json', config.GLOBAL_CHAT_PW, chat);
+
     await chat.sendMessage(response);
 })
 
